e7fbee5e7efc0925e198fceded65c6a7e6fb0373
Nirmeet <nirmeetparmar21@gmail.com>
2025-06-28 19:16:09 +0530

    nexus.js from main

--- nexus.js
+++ nexus.js
@@ -1,3 +1,3 @@
 console.log("Welcome toNexus session")
 console.log("Welcome to Git session")
-console.log("This is merge conflict from main branch")
+console.log("This is merge conflict from main branch, created from main")
